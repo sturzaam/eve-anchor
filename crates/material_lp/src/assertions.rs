583f70d2d2c8b75251b552c2a233556e73be31b7
Andrew Sturza <sturzaam@gmail.com>
2023-08-13 15:21:48 -0700

    panic when materials not in constellations

--- crates/material_lp/src/assertions.rs
+++ crates/material_lp/src/assertions.rs
@@ -1,4 +1,5 @@
-use crate::data::{CelestialResource, system_by_planet, get_celestial, get_constellation};
+use crate::data::{CelestialResource, celestial_resources_by_constellation, system_by_planet, get_celestial, get_constellation};
+use crate::Material;
 
 #[macro_export]
 macro_rules! assert_arrays_leq {
@@ -181,6 +182,77 @@ fn format_assert_planets_leq() {
     let panic_message = err.downcast_ref::<String>().unwrap();
     assert!(
         panic_message.contains("Count of planets 4 in KUSW-P exceeds 3"),
+        "Unexpected panic message: {}",
+        panic_message
+    );
+}
+
+#[macro_export]
+macro_rules! assert_materials_in_constellations {
+    ($materials:ident, $constellations:ident) => {
+        let mut panic_messages = Vec::new();
+        let mut found_in_any_constellation = false;
+
+        for material in $materials.iter() {
+            let mut material_found = false;
+
+            for constellation in $constellations.iter() {
+                let celestial_resources = celestial_resources_by_constellation(&constellation.0);
+                let matching_celestial_resources: Vec<_> = celestial_resources
+                .iter()
+                .filter(|celestial_resource| celestial_resource.resource_type_id == material.resource_type_id)
+                .collect();
+
+                if !matching_celestial_resources.is_empty() {
+                    material_found = true;
+                    found_in_any_constellation = true;
+                    break; // No need to check other constellations for this material
+                }
+            }
+
+            if !material_found {
+                let constellation_names: Vec<String> = $constellations.iter().map(|(name, _)| name.clone()).collect();
+                let panic_message = format!(
+                    "There is no known source of {} in {}",
+                    material.name,
+                    constellation_names.join(", ")
+                );
+                panic_messages.push(panic_message);
+            }
+        }
+
+        if !found_in_any_constellation {
+            let joined_panic_message = panic_messages.join("\n");
+            panic!("{}", joined_panic_message);
+        }
+    };
+}
+
+#[test]
+fn panic_when_materials_not_in_constellations() {
+    let materials: Vec<Material> = vec![
+        Material {
+            resource_type_id: 42001000007,
+            name: "Lucent Compound".into(),
+            quantity: 0,
+            valuation: 0.0
+        }
+    ];
+
+    let constellations: Vec<(String, i32)> = vec![
+        ("42-WDG".to_string(), 3),
+    ];
+    
+    let result = std::panic::catch_unwind(|| {
+        assert_materials_in_constellations!(materials, constellations);
+    });
+
+    
+    assert!(result.is_err(), "Expected a panic");
+    let err = result.unwrap_err();
+    let panic_message = err.downcast_ref::<String>().unwrap();
+    assert!(
+        panic_message.contains("There is no known source of Lucent Compound in 42-WDG"),
         "Unexpected panic message: {}",
         panic_message
     );
